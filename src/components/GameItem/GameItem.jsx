9328af1762462718815d1c59529b5c071e27a5e3
avngrd <vadimlazuk33@gmail.com>
2023-01-11 23:39:09 +0300

    added game pictures

--- src/components/GameItem/GameItem.jsx
+++ src/components/GameItem/GameItem.jsx
@@ -4,6 +4,9 @@ const GameItem = ({ game }) => {
   return (
     <div className="game-item">
       <div className="game-item__details">
+        <div className="game-cover">
+          <img src={game.image} alt={game.title} />
+        </div>
         <span className="game-item__title">{game.title}</span>
         <div className="game-item__genre">{game.genres.map((genre) => genre)}</div>
         <div className="game-item__buy">Buy</div>
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,7 +6,7 @@ import styles from './HomePage.module.scss';
 
 const GAMES = [
   {
-    image: 'src/assets/img/forza_5.jpeg',
+    image: 'img/forza_5.jpeg',
     title: 'Forza Horizon 5',
     genres: ['Гонки', 'Симулятор', 'Открытый мир'],
     price: 40,
@@ -16,7 +16,7 @@ const GAMES = [
       'Вас ждёт бесконечный калейдоскоп приключений Horizon! Совершайте увлекательные поездки по невероятно красивому и самобытному миру Мексики за рулём величайших автомобилей в истории. Начните своё приключение Horizon уже сегодня, добавив игру в свой список желаний!',
   },
   {
-    image: 'src/assets/img/battlefield_2042.jpg',
+    image: 'img/dota2.jpg',
     title: 'Dota 2',
     genres: ['Стратегия', 'Экшен', 'МОВА'],
     video: 'https://www.youtube.com/watch?v=Xx0YUuvW4wA',
@@ -26,7 +26,7 @@ const GAMES = [
       'Ежедневно миллионы игроков по всему миру сражаются от лица одного из более сотни героев Dota 2, и даже после тысячи часов в ней есть чему научиться. Благодаря регулярным обновлениям игра живёт своей жизнью: геймплей, возможности и герои постоянно преображаются.',
   },
   {
-    image: 'src/assets/img/life_is_strange_true_colors.jpeg',
+    image: 'img/life_is_strange_true_colors.jpeg',
     title: 'Life is Strange True Colors',
     genres: ['Глубокий сюжет', 'Протагонистка'],
     video: 'https://www.youtube.com/embed/b6CkzwVAr0M',
@@ -36,7 +36,7 @@ const GAMES = [
       'Алекс Чэнь от всех скрывает своё «проклятие» — сверхъестественную способность считывать сильные эмоции других и влиять на них. Но когда её брат погибает — якобы в результате несчастного случая, — Алекс использует её, чтобы узнать правду.',
   },
   {
-    image: 'src/assets/img/gta_v.jpeg',
+    image: 'img/gta_v.jpeg',
     title: 'Grand Theft Auto V',
     genres: ['Открытый мир', 'Экшен'],
     video: 'https://www.youtube.com/embed/QkkoHAzjnUs',
@@ -46,7 +46,7 @@ const GAMES = [
       'Grand Theft Auto V для PC позволяет игрокам исследовать знаменитый мир Лос-Сантоса и округа Блэйн в разрешении до 4k и выше с частотой 60 кадров в секунду.',
   },
   {
-    image: 'src/assets/img/rainbow_siege.jpeg',
+    image: 'img/rainbow_siege.jpeg',
     title: "Tom Clancy's Rainbow Six® Siege",
     video: 'https://www.youtube.com/embed/6wlvYh0h63k',
     genres: ['Тактика', 'Шутер'],
@@ -56,7 +56,7 @@ const GAMES = [
       "Tom Clancy's Rainbow Six Осада – это продолжение нашумевшего шутера от первого лица, разработанного студией Ubisoft Montreal.",
   },
   {
-    image: 'src/assets/img/assassins_creed_valhalla.png',
+    image: 'img/assassins_creed_valhalla.png',
     title: 'Assassin’s Creed Valhalla',
     genres: ['Паркур', 'РПГ', 'Открытый мир'],
     video: 'https://www.youtube.com/embed/ssrNcwxALS4',
