db0482148561e3609cc69781192d0bb61fb3fd9f
avngrd <vadimlazuk33@gmail.com>
2023-01-13 21:02:13 +0300

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { Routes, Route} from "react-router-dom";
-import HomePage from './pages/HomePage'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import { Provider } from 'react-redux';
 import './scss/app.scss';
+import { store } from './redux/store';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage/>}>
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </Provider>
   );
 }
 
