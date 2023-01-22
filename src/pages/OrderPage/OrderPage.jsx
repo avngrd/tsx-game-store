2e6ea0164263168a9b834768a40848a649be7089
avngrd <vadimlazuk33@gmail.com>
2023-01-22 20:41:58 +0300

    started order page

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { OrderItem } from '../../components/OrderItem/OrderItem';
+import { calcTotalPrice } from '../../components/utils/utils';
 
 export const OrderPage = () => {
   const items = useSelector((state) => state.cart.itemsInCart);
+
+  if (items.length < 1) {
+    return <h1>No orders</h1>;
+  }
 
   return (
     <div className="order-page">
@@ -11,6 +16,9 @@ export const OrderPage = () => {
         {items.map((game) => (
           <OrderItem game={game} />
         ))}
+      </div>
+      <div className="order-page__right">
+        <div className="order-page__total-price">{calcTotalPrice(items)}</div>
       </div>
     </div>
   );
